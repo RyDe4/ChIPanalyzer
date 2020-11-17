5b93b081c05d93558db8bb6aa8367e8d6913adcb
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2020-11-17 15:01:10 -0500

    improved testing

--- tests/testthat/test-findQuads.R
+++ tests/testthat/test-findQuads.R
@@ -3,7 +3,7 @@ library(ChIPAnalyzer)
 
 #correct sequence verified by examining sequence in IGV
 test_that("correct sequences retreived from genome", {
-  expect_equal(as.character(subject(findQuads(
+  expect_equal(as.character(Biostrings::subject(findQuads(
     bedPath = "MAZ_very_small_test.bed",
     seqWidth = 200,
     assembly = "hg19")[[1]])), "GGGGGAAGTGAGTGGATGTAGCTATTGGCAACCAGACAAGCAAAGCCAATTAAGGGGTGGAGAGCTTATGCAAAGAATGTGATGAATAAATTCTGGAACAAGAGTCCATGCCCTTTATTCATTTATAACAATTGAGGCAGTCAAGTGAGAAGCTGTAACATAGCATCAAATTATGATAGACCCAGATCACTATAACTGGA")
@@ -14,4 +14,11 @@ test_that("correct number of reports is generated", {
     bedPath = "MAZ_very_small_test.bed",
     seqWidth = 200,
     assembly = "hg19")), 20)
+})
+
+test_that("Error generated when invalid assembly is specified", {
+  expect_error(findQuads(
+    bedPath = "MAZ_very_small_test.bed",
+    seqWidth = 200,
+    assembly = "blah"))
 })
--- tests/testthat/test-getQuadMatrix.R
+++ tests/testthat/test-getQuadMatrix.R
@@ -1,10 +1,19 @@
 library(testthat)
 library(ChIPAnalyzer)
 
-test_that("returned matrix has correct dimensions", {
+test_that("check that returned matrix has correct dimensions", {
   testReports <- findQuads(
     bedPath = "MAZ_very_small_test.bed",
-    seqWidth = 200,
+    seq = 200,
     assembly = "hg19")
-  expect_equal(dim(getQuadMatrix(testReports, 200)), c(20, 200))
+  expect_equal(dim(getQuadMatrix(testReports)), c(20, 200))
 })
+
+test_that("check that an error occurs when the reports are for sequences of
+          different length", {
+  report1 <- pqsfinder::pqsfinder(Biostrings::DNAString("TGCCATAA"))
+  report2 <- pqsfinder::pqsfinder(Biostrings::DNAString("ACGACT"))
+  reportList <- list(report1, report2)
+  expect_error(getQuadMatrix(reportList))
+})
+
