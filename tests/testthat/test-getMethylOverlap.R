fd566024503fd13b261644e455ee81bfec7b6d0f
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2020-11-18 02:45:38 -0500

    adding note that test require full methylation dataset which is too big to be on github

--- tests/testthat/test-getMethylOverlap.R
+++ tests/testthat/test-getMethylOverlap.R
@@ -1,9 +1,21 @@
 library(testthat)
 library(ChIPAnalyzer)
 
+#this test requires the full methylation dataset which is too
+#big to be pushed to github
 test_that("getMethylOverlap does not return an impossibly large number
           of overlaps", {
   expect_equal(nrow(ChIPAnalyzer::getMethylOverlap(chipPath = "MAZ_very_small_test.bed",
                                     methylPath = "HEK293MethylData.bed")) <= 20000,
                TRUE)
+})
+
+#this test requires the full methylation dataset which is too
+#big to be pushed to github
+test_that("test that getMethylOverlap doesn't return an
+          empty dataframe when it has access to the full
+          methylation dataset", {
+            expect_equal(nrow(ChIPAnalyzer::getMethylOverlap(chipPath = "MAZ_very_small_test.bed",
+                                                             methylPath = "HEK293MethylData.bed")) <= 20000,
+                         TRUE)
 })
--- tests/testthat/test-methylIntegration1.R
+++ tests/testthat/test-methylIntegration1.R
@@ -1,6 +1,8 @@
 library(testthat)
 library(ChIPAnalyzer)
 
+#this test requires the full methylation dataset which is too big
+#to push to github
 test_that("The methylation Pie chart pipeline works", {
   testOverlap <- ChIPAnalyzer::getMethylOverlap(chipPath = "MAZ_very_small_test.bed",
                                  methylPath = "HEK293MethylData.bed")
