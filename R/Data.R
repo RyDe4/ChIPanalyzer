ed66f3a2b8e1af7a182d9576a0503a8d06d7f530
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2020-11-29 19:53:28 -0500

    Added biocViews: so dependency installation will work

---
+++ R/Data.R
@@ -0,0 +1,17 @@
+#' A BED File of DNA methylation data
+#'
+#' A BED file contianing the percentage of reads
+#' methylated(column 9) for HcfU cells
+#'
+#' @format BED file
+#'
+#' @source http://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeHaibMethylRrbs/
+
+#' A BED File of ChIP-SEQ peaks
+#'
+#' A BED file contianing the higher scoring ChIP-seq
+#' peaks for MAZ
+#'
+#' @format BED file
+#'
+#' @source https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE104247
