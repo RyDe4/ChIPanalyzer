f240e4d207ad89fe0ad29a136ea666d2e7947870
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2020-12-03 21:04:37 -0500

    Added Progress Bars

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -4,27 +4,36 @@ options(shiny.maxRequestSize=100*1024^2)
 ui <- fluidPage(
   tabsetPanel(
     tabPanel(title = "Plot G4 Distribution",
-      fileInput(inputId = "peaks",
-                label = "Choose ChIP-seq Peaks BED File"),
-      numericInput(inputId = "seqLen",
-                   label = "Length of sequence searched
-                   around Centre of Peaks",
-                   value = 100),
-      selectInput(inputId = "assembly", label = "Select Genome
-                  Assembly", choices = list("hg19", "hg38",
+      fluidRow(
+        column(6,
+        fileInput(inputId = "peaks",
+                  label = "Choose ChIP-seq Peaks BED File"),
+        numericInput(inputId = "seqLen",
+                     label = "Length of sequence searched
+                     around Centre of Peaks",
+                     value = 275),
+        selectInput(inputId = "assembly", label = "Select Genome
+                    Assembly", choices = list("hg19", "hg38",
                                             "mm9", "mm10")),
-      actionButton("runQuad",
-                   label = "Generate G4 Position Plot"),
-      plotOutput(outputId = "quadPlot")
+        actionButton("runQuad",
+                     label = "Generate G4 Position Plot")),
+      column(6,
+             plotOutput(outputId = "quadPlot"))
+      )
     ),
     tabPanel(title = "Plot Methylation Percentage",
-      fileInput(inputId = "methylPeaks",
-        label = "Choose ChIP-seq Peaks BED File"),
-      fileInput(inputId = "methylData",
-        label = "Choose Methylation Data BED File"),
-      actionButton("runMethylOverlap",
-               label = "Generate G4 Position Plot"),
-      plotOutput(outputId = "methylPlot")
+      fluidRow(
+        column(6,
+          fileInput(inputId = "methylData",
+                label = "Choose Methylation Data BED File"),
+          fileInput(inputId = "methylPeaks",
+          label = "Choose ChIP-seq Peaks BED File"),
+        actionButton("runMethylOverlap",
+                label = "Generate G4 Position Plot")
+        ),
+        column(6, plotOutput(outputId = "methylPlot")
+        )
+      )
     )
   )
 )
@@ -32,30 +41,36 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   getPerc <- function (bedPath, seqWidth, assemblyVersion) {
+    incProgress(0.1, detail = "Finding Quadruplexes")
     reports <- findQuads(bedPath = bedPath,
                          seqWidth = seqWidth,
                          assemblyVersion = assemblyVersion)
+    incProgress(0.7, detail = "Formatting Postitions")
     qMatrix <- getQuadMatrix(quadReports = reports)
+    incProgress(0.9, detail = "Calculating Percentages")
     quadCoveragePercentage <- getQuadCoveragePercentage(quadMatrix = qMatrix)
     return(quadCoveragePercentage)
   }
 
   quadCoveragePercentage <- eventReactive(input$runQuad, {
+    withProgress(message = "Generating Plot...", value = 0, {
     getPerc(input$peaks$datapath, input$seqLen, input$assembly)
+    })
   })
 
   output$quadPlot <- renderPlot({
-    plotQuadPosition(quadCoveragePercentage(), "")
+      plotQuadPosition(quadCoveragePercentage(), "")
   })
 
   overlap <- eventReactive(input$runMethylOverlap, {
-    getMethylOverlap(input$methylPeaks$datapath, input$methylData$datapath)
+    withProgress(message = "Finding peak sites with Methylation Data...",
+                 value = 0, {
+      getMethylOverlap(input$methylPeaks$datapath, input$methylData$datapath)
+    })
   })
 
   output$methylPlot <- renderPlot({
-    if (!is.null(overlap())) {
       plotMethylPercentage(overlap())
-    }
   })
 }
 
