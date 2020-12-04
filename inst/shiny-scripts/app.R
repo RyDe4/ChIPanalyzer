229a44b97edd4252260fef3b7e75a15d35ede985
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2020-12-03 19:13:08 -0500

    added more features to shiny app

--- inst/shiny-scripts/app.R
+++ inst/shiny-scripts/app.R
@@ -1,27 +1,60 @@
 library(shiny)
+options(shiny.maxRequestSize=100*1024^2)
 
 ui <- fluidPage(
-  fileInput(inputId = "peaks",
-            label = "Choose ChIP-seq Peaks BED File"),
-  numericInput(inputId = "seqLen",
-               label = "Length of sequence searched
-               around Centre of Peaks",
-               value = 100),
-  selectInput(inputId = "assembly", label = "Select Genome
-              Assembly", choices = list("hg19", "hg38",
-                                        "mm9", "mm10")),
-  plotOutput(outputId = "quadPlot")
+  tabsetPanel(
+    tabPanel(title = "Plot G4 Distribution",
+      fileInput(inputId = "peaks",
+                label = "Choose ChIP-seq Peaks BED File"),
+      numericInput(inputId = "seqLen",
+                   label = "Length of sequence searched
+                   around Centre of Peaks",
+                   value = 100),
+      selectInput(inputId = "assembly", label = "Select Genome
+                  Assembly", choices = list("hg19", "hg38",
+                                            "mm9", "mm10")),
+      actionButton("runQuad",
+                   label = "Generate G4 Position Plot"),
+      plotOutput(outputId = "quadPlot")
+    ),
+    tabPanel(title = "Plot Methylation Percentage",
+      fileInput(inputId = "methylPeaks",
+        label = "Choose ChIP-seq Peaks BED File"),
+      fileInput(inputId = "methylData",
+        label = "Choose Methylation Data BED File"),
+      actionButton("runMethylOverlap",
+               label = "Generate G4 Position Plot"),
+      plotOutput(outputId = "methylPlot")
+    )
+  )
 )
 
+
 server <- function(input, output) {
+  getPerc <- function (bedPath, seqWidth, assemblyVersion) {
+    reports <- findQuads(bedPath = bedPath,
+                         seqWidth = seqWidth,
+                         assemblyVersion = assemblyVersion)
+    qMatrix <- getQuadMatrix(quadReports = reports)
+    quadCoveragePercentage <- getQuadCoveragePercentage(quadMatrix = qMatrix)
+    return(quadCoveragePercentage)
+  }
+
+  quadCoveragePercentage <- eventReactive(input$runQuad, {
+    getPerc(input$peaks$datapath, input$seqLen, input$assembly)
+  })
+
   output$quadPlot <- renderPlot({
-    if (!is.null(input$peaks)) {
-      reports <- findQuads(bedPath = input$peaks$datapath,
-                          seqWidth = input$seqLen,
-                          assemblyVersion = input$assembly)
-      qMatrix <- getQuadMatrix(quadReports = reports)
-      quadCoveragePercentage <- getQuadCoveragePercentage(quadMatrix = qMatrix)
-      plotQuadPosition(quadCoveragePercentage, "")
+    plotQuadPosition(quadCoveragePercentage(), "")
+  })
+
+  overlap <- eventReactive(input$runMethylOverlap, {
+    getMethylOverlap(input$methylPeaks$datapath, input$methylData$datapath)
+  })
+
+  output$methylPlot <- renderPlot({
+    if (!is.null(overlap())) {
+      plotMethylPercentage(overlap())
     }
   })
 }
