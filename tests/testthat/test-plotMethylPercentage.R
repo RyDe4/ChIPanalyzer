116024b02950a2bb34ed5a0de71a1ca823a3b251
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2020-12-08 21:48:59 -0500

    improved testing for methylation plot

--- tests/testthat/test-plotMethylPercentage.R
+++ tests/testthat/test-plotMethylPercentage.R
@@ -2,5 +2,12 @@ library(testthat)
 library(ChIPAnalyzer)
 
 test_that("Error occurs when empty data frame passed in", {
-  expect_error(plotMethylPercentage(data.frame()))
+  expect_error(typeof(plotMethylPercentage(data.frame())),
+               "empty dataframe provided")
+})
+
+test_that("Test that function returns object of correct type", {
+  overlap <- getMethylOverlap("MAZ_very_small_test.bed", "HcfUMethylData.bed")
+  expect_equal(typeof(plotMethylPercentage(overlap)),
+               "list")
 })
