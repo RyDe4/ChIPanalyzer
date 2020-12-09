4a92171f2d23b6f476d251139594c490497b60ae
Ryan Denniston <ryan.denniston@mail.utoronto.ca>
2020-12-08 22:11:50 -0500

    Improved G-quadruplex plot tests

--- tests/testthat/test-plotQuadPosition.R
+++ tests/testthat/test-plotQuadPosition.R
@@ -3,8 +3,8 @@ library(ChIPAnalyzer)
 
 test_that("test that ploting valid vector
           doesn't cause error", {
-  expect_equal(plotQuadPosition(c(10, 20, 30, 20, 10),
-                                "test quad plot"), TRUE)
+  expect_equal(typeof(plotQuadPosition(c(10, 20, 30, 20, 10),
+                                       "test title")), "list")
 })
 
 test_that("test that attempting to plot empty dataframe
